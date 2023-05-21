6f6afe233ad892b8c92fc09de7c7ae0aeafe6677
Messi Chiquito <luvelezg@unal.edu.co>
2023-05-20 21:44:20 -0500

    Integrado Footer.jsx

---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+<footer class="container py-5">
+    <div class="row">
+        <div class="col-6 col-md">
+            <h5>React</h5>
+        </div>
+        <div class="col-6 col-md">
+            <h5>Luis Felipe Vélez García</h5>
+        </div>
+        <div class="col-6 col-md">
+            <h5>Juan Esteban Valderrama</h5>
+        </div>
+        <div class="col-6 col-md">
+            <h5>Santiago Martinez</h5>
+        </div>
+    </div>
+</footer>
