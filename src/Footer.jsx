516bf56ded2d2067e3ca1dd05109da51fab39ab3
Messi Chiquito <luvelezg@unal.edu.co>
2023-05-22 22:12:58 -0500

    Se creo el Header y Footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,16 +1,26 @@
-<footer class="container py-5">
-    <div class="row">
-        <div class="col-6 col-md">
-            <h5>React</h5>
-        </div>
-        <div class="col-6 col-md">
-            <h5>Luis Felipe Vélez García</h5>
-        </div>
-        <div class="col-6 col-md">
-            <h5>Juan Esteban Valderrama</h5>
-        </div>
-        <div class="col-6 col-md">
-            <h5>Santiago Martinez</h5>
-        </div>
-    </div>
-</footer>
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function Footer() {
+    return (
+        <footer class="footer fixed-bottom bg-light">
+            <div class="row">
+                <div class="col-6 col-md">
+                    <h5>React</h5>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>Luis Felipe Velez</h5>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>Juan Esteban Valderrama</h5>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>Santiago Martinez</h5>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
-import './index.css'
+import Footer from './Footer.jsx'
+import Header from './Header.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>,
 )
